efde2b503eff2def54f861555adf2699f71fe4e6
xcoder123 <raivucis@gmail.com>
2016-12-03 14:39:25 +0000

    First working iteration of "dzivais Tv"
    Designed the data structure to allow direct links or in some cases to be
    able to call special functions for resolving the url. At the moment 5
    the mos popular channels are available. Where two of them have sources
    which work only in Latvia

--- plugin.video.dzivaistv/default.py
+++ plugin.video.dzivaistv/default.py
@@ -1,15 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import re
 import xbmc
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 import urlresolver
 import urllib, urllib2, ssl, re
 import CommonFunctions
 import requests
 
-mainURL = 'https://cinemalive.tv'
+channelList = {'Latvijas Kanāli': {
+									'icon': '%s/Latvia.png',
+									'channels':	[{'name': 'LTV1',
+												'thumb': '%s/ltv1.png',
+												'sources': ['resolve_ltv1_source1']
+												},
+												{'name': 'LTV7',
+												'thumb': '%s/ltv7.png',
+												'sources': ['resolve_ltv7_source1']
+												},
+												{'name': 'TV3 Latvija',
+												'thumb': '%s/tv3.png',
+												'sources': ['http://wpc.11eb4.teliasoneracdn.net/8011EB4/origin1/tvplay/mtgstream2_medium.stream/chunklist.m3u8']
+												},
+												{'name': 'LNT',
+												'thumb': '%s/lnt.png',
+												'sources': ['http://wpc.11eb4.teliasoneracdn.net/8011EB4/origin1/tvplay/mtgstream1_medium.stream/playlist.m3u8']
+												},
+												{'name': 'TV6 Latvija',
+												'thumb': '%s/tv6.png',
+												'sources': ['http://wpc.11eb4.teliasoneracdn.net/8011EB4/origin1/tvplay/mtgstream4_medium.stream/playlist.m3u8']
+												}]
+								  }
+			  }
+							  
+mysettings = xbmcaddon.Addon(id = 'plugin.video.dzivaistv')
+profile = mysettings.getAddonInfo('profile')
+home = mysettings.getAddonInfo('path')
+getSetting = xbmcaddon.Addon().getSetting
+iconpath = xbmc.translatePath(os.path.join(home, 'resources/icons/'))
+
 
 def showkeyboard(txtMessage="",txtHeader="",passwordField=False):
     if txtMessage=='None': txtMessage=''
@@ -20,23 +53,6 @@ def showkeyboard(txtMessage="",txtHeader="",passwordField=False):
     else:
         return False # return ''
 		
-def Search():
-	text = showkeyboard('', u'Meklēt filmu')
-	print "Search string: " + text
-	post_fields = {'search': text}
-	html = postHTML("https://cinemalive.tv/scripts/search.php", post_fields)
-	
-	print html
-	# found_items = common.parseDOM(html, "div", attrs = { "style": "height:78px" })
-	found_links = common.parseDOM(html, "a", ret = "href")
-	found_name = common.parseDOM(html, "span", attrs = { "style": "color:#bcbcbc" })
-	found_image = common.parseDOM(html, "img", ret = "src")
-	print found_links
-	print found_name
-	print found_image
-	
-	for i in range(0, len(found_links)):
-		addDir(found_name[i].encode('utf-8'), found_links[i], 'state_play', found_image[i].replace(mainURL, "").replace('xs.jpg', 'md.jpg') )
 		
 def postHTML(url, post_fields):
 
@@ -46,28 +62,12 @@ def postHTML(url, post_fields):
 		html = r.text.encode('utf-8')
 	else:
 		print "Crap we have the old version"
-		
-		# hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-		# 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		# 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-		# 'Accept-Encoding': 'none',
-		# 'Accept-Language': 'pl-PL,pl;q=0.8',
-		# 'Connection': 'keep-alive'}
+	
 		
 		# http://dev.morf.lv/mirror.php?url=https://cinemalive.tv/scripts/search.php&post=
 		postParam = ""
 		for key, value in post_fields.iteritems():
 			postParam+=key+":"+value
-			
-		# req = urllib2.Request("http://dev.morf.lv/mirror.php?url="+url+"&post="+postParam, headers=hdr)
-		
-		# try:
-			# page = urllib2.urlopen(req)
-		# except urllib2.HTTPError, e:
-			# print e.fp.read()
-		
-		# html = page.read()
-		# print html
 		
 		r = requests.get("http://dev.morf.lv/mirror.php?url="+url+"&post="+postParam)
 		print(r.status_code, r.reason)
@@ -134,95 +134,78 @@ def getHTML(url, data = False):
 	print "URL Downloaded"
 	return html
 	
-def Categories():
-	url = 'http://cinemalive.tv/filmaslatviski'
-	html = getHTML(url)
-	print 'html: ' + html
-	genre_links = common.parseDOM(html, "ul", attrs = { "class": "nav-submenu" })
-	addDir('Meklēt', '', 'state_search', None)	
-	print genre_links
 	
-# for link, title in genre_links:
-# if isLinkUseful(link):
-# addDir(title, url + link, 20, None)
+def resolve_ltv1_source1():
+	html = getHTML('http://embed.ls.lv/ltv1g/index.php?utm_medium=ltv1')
+	searchObj = re.search('file: ".*"', html)
+	if searchObj:
+		resolvedUrl = searchObj.group().replace('file: "', '')
+		resolvedUrl = resolvedUrl[:-1]
+		return resolvedUrl
+	else:
+	   return False
+	
+def resolve_ltv7_source1():
+	html = getHTML('http://embed.ls.lv/ltv2g/index.php?utm_medium=ltv2')
+	searchObj = re.search('file: ".*"', html)
+	if searchObj:
+		resolvedUrl = searchObj.group().replace('file: "', '')
+		resolvedUrl = resolvedUrl[:-1]
+		return resolvedUrl
+	else:
+	   return False
+	
 
-def isLinkUseful(needle):
-    haystack = ['/index.php', '#']
-    return needle not in haystack
+def get_categories():
+         """
+         Get's the list of channel categories: Latvian, Russian, Sport etc.
+         """
+         return channelList.keys()
+		 
+def get_sources(category, channel):
+	channels = channelList[category]['channels']
 	
-def isVideoFormat(needle):
-    haystack = ['mp4', 'avi']
-    return needle in haystack
+	for ch in channels:
+		if ch['name'] == channel:
+			return ch['sources']
+			
+	return
 	
+def isURL(url):
+	try:
+		f = urllib2.urlopen(url)
+		return True
+	except ValueError:  # invalid URL
+		return False
+		 
 def HomeNavigation():
-	# url = 'https://cinemalive.tv/filmaslatviski'
-	# html = getHTML(url)
-	# print 'html: ' + html
-	# nav_links_list = common.parseDOM(html, "div", attrs = { "id": "genre-nav" })
-	# nav_links = common.parseDOM(nav_links_list, "a", ret = "href")
-	# nav_links_name = common.parseDOM(nav_links_list, "a")
-	addDir('TV3 Latvija', '', 'state_tv3', None)
-	addDir('Meklēt', '', 'state_search', None)
-		
-	# pagirasList = u'https://openload.co/embed/pbQKtANhUNI/'	
-	# link = urlresolver.resolve(pagirasList)
-	# addDir('Paģiras 2', pagirasList, 'state_play', None)
-	# addLink("Paģiras 2", link.encode('utf-8'), None)
-	# print nav_links
-	# print nav_links_name
-	# for i in range(0, len(nav_links)):
-		# if isLinkUseful(nav_links[i]):
-			# print mainURL + nav_links[i]
-			# addDir(nav_links_name[i].encode('utf-8'), mainURL + nav_links[i], 'state_movies', None)
-			
-def Movies(url, page=1):
-	html = getHTML(url+"/lapa/"+str(page))
-	# print "html" + url
-	moviesList = common.parseDOM(html, "div", attrs = { "class": "base-used" })
-	moviesTitleList = common.parseDOM(moviesList, "h2")
-	moviesThumbnailURLsList = common.parseDOM(moviesList, "img", attrs = { "class": "img-thumbnail" }, ret = "src")
-	moviesURLs = common.parseDOM(moviesList, "a", ret = "href")
-	# print moviesThumbnailURLsList
-	
-	
-	for i in range(0, len(moviesURLs)):
-		addDir(moviesTitleList[i].encode('utf-8'), moviesURLs[i], 'state_play', moviesThumbnailURLsList[i])
-		
-	if len(moviesURLs) >= 50:
-		addDir("Nākamā Lapa >>", url , 'state_movies', None, str(int(page) + 1))
+	categories = get_categories()
+	for category in categories:
+		addDir(category, category, 'state_channels', channelList[category]['icon']% iconpath)
 
-
-def PlayMovie(url, title, picture):
-	print "url: " + url
-	html = getHTML(url)
-	print "html: " + html
 	
-	mainMovieCol = common.parseDOM(html, "div", attrs = { "class": "row mov-com-col"} )
-	print mainMovieCol
-	videoContainers = common.parseDOM(mainMovieCol[0], "source", ret="src")
-	videoContainers = videoContainers + common.parseDOM(mainMovieCol[0], "iframe", ret="src")
-	print "ATRASTIE VIDEO: "
-	print videoContainers
-	print title.decode('latin-1').encode('utf-8')
+def Channels(category):
+	channels = channelList[category]['channels']
+	for channel in channels:
+		addDir(channel['name'], category+":"+channel['name'], 'state_sources', channel['thumb']% iconpath)
+		
+def Sources(url):
+	splitParam = url.split(":")
 	
-	if len(videoContainers) > 0:		
-		link = urlresolver.resolve(videoContainers[0])
-		if link != False:
-			addLink(title.decode('utf-8').encode('utf-8') + " - Latviski", link.encode('utf-8'), picture)	
-		elif isVideoFormat(videoContainers[0].split(".")[-1]):
-			addLink(title.decode('utf-8').encode('utf-8') + " - Latviski", videoContainers[0], picture)	
-		print link
-	if len(videoContainers) > 1:
-		link = urlresolver.resolve(videoContainers[1])
-		if link != False:
-			addLink(title.decode('utf-8').encode('utf-8') + " - Angliski", link.encode('utf-8'), picture)
-		elif isVideoFormat(videoContainers[1].split(".")[-1]):
-			addLink(title.decode('utf-8').encode('utf-8') + " - Latviski", videoContainers[1], picture)	
+	category = splitParam[0]
+	channel = splitParam[1]
+	print category, channel
+	sources = get_sources(category, channel)
+	
+	for i in range(0, len(sources)):
+		print isURL(sources[i]), sources[i]
+		if isURL(sources[i]) == False:
+			resolvedUrl = globals()[sources[i]]()
+			if resolvedUrl != False:
+				addLink(u"Tiešraide %s"% (i+1), resolvedUrl, None)
+		else:
+			addLink(u"Tiešraide %s"% (i+1), sources[i], None)
 			
-		print link
-	# link = re.compile('file:[\s\t]*"(.+?)"').findall(html.decode('windows-1251').encode('utf-8'))[0]
-	
-	
 	
 	
 
@@ -231,7 +214,7 @@ def addDir(title, url, mode, picture, page=None):
     if  picture == None:
         item = xbmcgui.ListItem(title, iconImage='DefaultFolder.png', thumbnailImage='')
     else:
-        item = xbmcgui.ListItem(title, iconImage='DefaultFolder.png' , thumbnailImage=mainURL + picture)    
+        item = xbmcgui.ListItem(title, iconImage='DefaultFolder.png' , thumbnailImage=picture)    
         sys_url += '&picture=' + urllib.quote_plus(str(picture))
     if page != None:
         sys_url += '&page=' + urllib.quote_plus(str(page))
@@ -308,18 +291,10 @@ except:
 
 if mode == None:
 	HomeNavigation();
-elif mode == 'state_movies':
-	Movies(url, page)
-elif mode == 'state_play':
-	PlayMovie(url, title, picture)
-elif mode == 'state_search':
-	Search()
-elif mode == 'state_tv3':
-	url = 'http://wpc.11eb4.teliasoneracdn.net/8011EB4/origin1/tvplay/mtgstream2_medium.stream/chunklist.m3u8'
-	# url = "C:\Users\Raivis\Downloads\chunklist (3).m3u8"
-	playlistFile = getHTML('http://wpc.11eb4.teliasoneracdn.net/8011EB4/origin1/tvplay/mtgstream2_high.stream/chunklist.m3u8')
-	play_stream(url)
-	# print playlistFile
+elif mode == 'state_channels':
+	Channels(url)
+elif mode =='state_sources':
+	Sources(url)
 
 		
 
